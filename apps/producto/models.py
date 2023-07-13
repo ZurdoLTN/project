eee287fab3df310b802685d149663ec7c295e1d6
Augusto Salgado <augustocesarsalgado05@gmail.com>
2023-07-13 16:57:33 -0300

    creando models en app producto

--- apps/producto/models.py
+++ apps/producto/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    precio = models.IntegerField()
