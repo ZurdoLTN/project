76f9c2e0856d22ef0e30369bfaec11cdd1533a8b
Augusto Salgado <augustocesarsalgado05@gmail.com>
2023-07-15 22:27:09 -0300

    Modificacion de views en app producto

--- apps/producto/views.py
+++ apps/producto/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Producto
+from django.http import HttpRequest, HttpResponse
+
+from .forms import ProductoForm
 
 def home(request):
-    contexto = {"app": "Producto"}
-    return render(request, "producto/index.html", contexto)
+    productos_registrados = Producto.objects.all()
+    return render(request, "producto/index.html", {"producto": productos_registrados})
+
+# Formulario para ingresar productos
+
+def ingresar_productos(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("producto:home")
+    else:
+        form = ProductoForm()
+    return render(request, "producto/ingresar_productos.html", {"form": form})
+        
+   
+
+
+
+
