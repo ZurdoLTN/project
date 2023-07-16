3409fceb4568c43a5d4b75452f34140eec649df9
Augusto Salgado <augustocesarsalgado05@gmail.com>
2023-07-15 23:39:19 -0300

    creando funcion crear_cliente

--- apps/cliente/views.py
+++ apps/cliente/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django import http
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
 
 from .forms import ClienteForm
 
@@ -10,6 +10,16 @@ from .models import Cliente, Pais
 
 def home(request):
     clientes_registros = Cliente.objects.all()
-    context = {"cliente": clientes_registros}
-    return render(request, "cliente/index.html", context)
+    return render(request, "cliente/index.html", {"cliente": clientes_registros})
+
+def crear_cliente(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("cliente:home")
+    else:
+        form = ClienteForm()
+    return render(request, "cliente/crear_cliente.html", {"form": form})
+    
 
